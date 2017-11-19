ccc31722ec7cf3206fa16e96be0ecf37086887d9
Haichao Yu <haichaoy@asu.edu>
2017-11-19 16:40:46 -0700

    Initial commit

---
+++ src/main/java/com/yhc/brainetserver/BrainetServerApplication.java
@@ -0,0 +1,12 @@
+package com.yhc.brainetserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainetServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainetServerApplication.class, args);
+	}
+}
